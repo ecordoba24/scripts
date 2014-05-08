980ebf004a87a242b93efac9acf70c98ab6cc01d
Eugenio Cordoba <eugenio@nexorone.com>
2014-05-08 13:35:52 -0500

    solo mostrar todos los errores en localhost

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
-<?php 
+<?php
 
-init_set('display_errors', true);
+if($_SERVER["SERVER_NAME"] == "localhost"){
+    $show = true;
+}else{
+    $show = false;
+}
+init_set('display_errors', $show);
 error_reporting(E_ALL);
