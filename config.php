89757a8a9bf640ac14ccbc78ca91848dfb5eb478
Eugenio Cordoba <e.cordoba.24@gmail.com>
2015-11-25 11:21:41 -0500

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ if($_SERVER["SERVER_NAME"] == "localhost"){
     $show = false;
 }
 ini_set('display_errors', $show);
-error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ALL & ~E_NOTICE  & ~E_STRICT);
 
 if($_SERVER["SERVER_NAME"] == "localhost"){
     echo "<script>console.log('Nombre de base de datos: {$nombre_db}');</script>";
