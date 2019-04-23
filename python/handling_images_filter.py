343553129acb20238dd4338ea374bf504e65f987
eu <eu@localhost.localdomain>
2019-04-23 17:40:49 -0400

    Agregar filtro a imagen

---
+++ python/handling_images_filter.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from PIL import ImageFilter
+
+try:
+    img = Image.open('images/test.jpg')
+    # Abrir una imagen
+
+    img.filter( ImageFilter.FIND_EDGES ).save('images/test_filter.jpg')
+    # agregar filtro a la imagen y guardar
+    
+except IOError:
+    print( 'No se encontro imagen' )
+
+
+# Requisitos:
+# pip install Pillow
+# https://pillow.readthedocs.io/en/stable/
